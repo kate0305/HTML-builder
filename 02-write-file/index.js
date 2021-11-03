23d66d3b86e82c3f07417e9000385c1f29026648
Katsiaryna Voronich <katarinka403@gmail.com>
2021-11-04 02:57:52 +0300

    feat: add solution write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const process = require('process');
+
+const stdin = process.stdin;
+
+const pathFile = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(pathFile);  //Создание потока записи в текстовый файл
+
+console.log('Hello! Enter your text:'); 
+
+const rl = readline.createInterface({ input: stdin, output });
+
+rl.on('line', (text) => {
+  if (text.trim() === 'exit') {
+    console.log('\nGood luck learning Node.js!');
+    rl.close();
+  }
+  output.write(`${text}\n`);
+});
+
+//остановка процесса нажатием CTRL + C
+process.on('SIGINT', () => {
+  console.log('\nGood luck learning Node.js!');
+  process.exit();
+});
