d279c24fbc7d4306f865f75545822c4fcaa449b6
Katsiaryna Voronich <katarinka403@gmail.com>
2021-11-06 01:22:53 +0300

    feat: add solution merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+const path = require('path');
+
+const pathDir = path.join(__dirname, 'styles');
+const pathOutput = path.join(__dirname, 'project-dist', 'bundle.css');
+const output = fs.createWriteStream(pathOutput);
+
+async function createBundle() {
+  try {
+  const files = await readdir(pathDir, {withFileTypes: true});
+  for (const file of files) {
+    const fileExtension = path.extname(file.name).slice(1);
+    if (file.isFile() && fileExtension === 'css') {
+        const pathInput = path.join(pathDir, file.name);
+        const input = fs.createReadStream(pathInput);
+        input.pipe(output);
+    }
+  }
+  } catch (err) {
+    console.log(err);
+  }
+}
+createBundle();
